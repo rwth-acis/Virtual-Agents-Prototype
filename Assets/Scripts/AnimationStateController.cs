058ded84877e53a74f9dc4b47daacbd40234b669
Danylo Bekhter <danylo.bekhter@rwth-aachen.de>
2021-04-21 10:27:59 +0200

    Imported Mixamo animations, added animation control to the characters
    Also deleted the unnecessary half-body RPM avatar from the scene

---
+++ Assets/Scripts/AnimationStateController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+          animator = GetComponent<Animator>();
+          Debug.Log(animator);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          if (Input.GetKey("w"))
+          {
+                animator.SetBool("isWalking", true);
+                Debug.Log("Player pressed \"w\"");
+          }
+          if (Input.GetKey("s"))
+          {
+                animator.SetBool("isRunning", true);
+                Debug.Log("Player pressed \"s\"");
+          }
+          else
+          {
+                animator.SetBool("isRunning", false);
+                animator.SetBool("isWalking", false);
+          }
+    }
+}
