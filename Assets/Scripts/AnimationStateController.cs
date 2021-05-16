1332d0d478d087b004125ff68ad97294d8636336
Danylo Bekhter <danylo.bekhter@rwth-aachen.de>
2021-05-16 09:04:21 +0200

    Working in-place running, walking and idling animations for both characters

--- Assets/Scripts/AnimationStateController.cs
+++ Assets/Scripts/AnimationStateController.cs
@@ -19,18 +19,21 @@ public class AnimationStateController : MonoBehaviour
           if (Input.GetKey("s"))
           {
                 animator.SetBool("isWalking", true);
-                Debug.Log("Player pressed \"s\" and invoked isWalking = \"" + animator.GetBool("isWalking") + "\"");
+                //Debug.Log("Player pressed \"s\" and invoked isWalking = \"" + animator.GetBool("isWalking") + "\"");
           }
           if (Input.GetKey("w"))
           {
                 animator.SetBool("isRunning", true);
-                Debug.Log("Player pressed \"w\" and invoked isRunning = \"" + animator.GetBool("isRunning") + "\"");
+                //Debug.Log("Player pressed \"w\" and invoked isRunning = \"" + animator.GetBool("isRunning") + "\"");
           }
-          else if (!Input.GetKey("s") && !Input.GetKey("w"))
+          if (!Input.GetKey("s"))
           {
                 animator.SetBool("isWalking", false);
+                //Debug.Log("Nothing is pressed, so isRunning == \"" + animator.GetBool("isRunning") + "\" and isWalking == \"" + animator.GetBool("isWalking") + "\"");
+          }
+          if (!Input.GetKey("w"))
+          {
                 animator.SetBool("isRunning", false);
-                Debug.Log("Nothing is pressed, so isRunning == \"" + animator.GetBool("isRunning") + "\" and isWalking == \"" + animator.GetBool("isWalking") + "\"");
           }
     }
 }
