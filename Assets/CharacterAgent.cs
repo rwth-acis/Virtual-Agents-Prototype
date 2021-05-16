1587b454607bc314462beb3cf02ba3e8f5d668ac
Danylo Bekhter <danylo.bekhter@rwth-aachen.de>
2021-05-16 14:22:52 +0200

    Prepared the scene for navigation using NavMesh, added a NavMesh Agent and a script for its movement

---
+++ Assets/CharacterAgent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// NavMesh
+using UnityEngine.AI;
+
+public class CharacterAgent : MonoBehaviour
+{
+    public GameObject destination;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(destination.transform.position);
+    }
+}
