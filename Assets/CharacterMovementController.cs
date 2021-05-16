85f03624b7e3307724621bb1b0f58ec22f84de17
Danylo Bekhter <danylo.bekhter@rwth-aachen.de>
2021-05-16 09:39:08 +0200

    Added a CharacterController component with a simple forward movement

---
+++ Assets/CharacterMovementController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementController : MonoBehaviour
+{
+    private CharacterController characterController;
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        characterController.SimpleMove(Vector3.forward * speed);
+    }
+}
