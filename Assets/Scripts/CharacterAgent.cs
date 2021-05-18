8be7d5d4f747926c801401447542e0eed8c83aae
Danylo Bekhter <danylo.bekhter@rwth-aachen.de>
2021-05-18 15:44:15 +0200

    Tried to animate character's movement
    There is a bug where the character is constantly crouching

--- Assets/Scripts/CharacterAgent.cs
+++ Assets/Scripts/CharacterAgent.cs
@@ -3,20 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 // NavMesh
 using UnityEngine.AI;
+// Third person animation
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class CharacterAgent : MonoBehaviour
 {
     public GameObject destination;
-    NavMeshAgent agent;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
     // Start is called before the first frame update
     void Start()
     {
-        agent = gameObject.GetComponent<NavMeshAgent>();
+        //agent = gameObject.GetComponent<NavMeshAgent>();
+        // Disable agent rotation updates, since they are handled by the character
+        agent.updateRotation = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(destination.transform.position);
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            // Character's movement is based on agent's desired movement. "False" for no crouching and no jumping
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
     }
 }
