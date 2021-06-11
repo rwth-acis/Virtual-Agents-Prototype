4fcb7684b969d3de82eaa354a95bb20ba23f81af
Danylo Bekhter <danylo.bekhter@rwth-aachen.de>
2021-06-11 11:14:35 +0200

    Character's animation speed now automatically adjusts to its proxy object's movement speed

--- Assets/Scripts/CharacterAgent.cs
+++ Assets/Scripts/CharacterAgent.cs
@@ -11,8 +11,9 @@ public class CharacterAgent : MonoBehaviour
     public GameObject destination;
     public NavMeshAgent agent;
     public ThirdPersonCharacter character;
-    public float speed = 1;
-    public Rigidbody rigidBody;
+    private const float damping = 8;
+    private Vector3 previousPosition;
+    private float curSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,9 +26,16 @@ public class CharacterAgent : MonoBehaviour
     void Update()
     {
         agent.SetDestination(destination.transform.position);
+
+        Vector3 curMove = transform.position - previousPosition;
+        curSpeed = curMove.magnitude / Time.deltaTime;
+        previousPosition = transform.position;
+        Debug.Log(curSpeed);
+
         if (agent.remainingDistance > agent.stoppingDistance)
         {
-            if(!rigidBody.IsSleeping())
+            character.Move(agent.desiredVelocity * curSpeed/damping, false, false);
+            /*if(!rigidBody.IsSleeping())
             {
                 // Character's movement is based on agent's desired movement. "False" for no crouching and no jumping
                 character.Move(agent.desiredVelocity * speed, false, false);
@@ -36,7 +44,8 @@ public class CharacterAgent : MonoBehaviour
             {
                 // In case the agent is not actually moving, such as when their movement is hindered by a wall
                 character.Move(Vector3.zero, false, false);
-            }
+            }*/
+
         }
         else
         {
