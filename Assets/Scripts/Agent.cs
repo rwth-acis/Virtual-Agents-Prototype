aa3bf3905cd3c6d7715e4a64553bbe0ea1abb46f
Danylo Bekhter <danylo.bekhter@rwth-aachen.de>
2021-06-11 12:19:21 +0200

    Laid out the structure for the Agent class

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,41 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+namespace VirtualAgentsFramework
+{
+    public class Agent : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void WalkTo()
+        {
+
+        }
+
+        void RunTo()
+        {
+
+        }
+
+        void PlayAnimation()
+        {
+
+        }
+
+        void PickUp()
+        {
+
+        }
+    }
+}
