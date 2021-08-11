1a3b2586ee13f5b9d820772a6c7e6e2708837fef
Danylo Bekhter <danylo.bekhter@rwth-aachen.de>
2021-08-11 11:48:12 +0300

    Moved necessary files to package folder

---
+++ Assets/Virtual Agents Framework/Runtime/Scripts/SampleAgentController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using VirtualAgentsFramework.AgentTasks;
+
+namespace VirtualAgentsFramework
+{
+    public class SampleAgentController : MonoBehaviour
+    {
+        [SerializeField] Agent agent;
+        [SerializeField] GameObject destination;
+
+        void Start()
+        {
+            agent.WaitForSeconds(2f);
+            agent.WalkTo(destination);
+        }
+
+        void Update() {}
+    }
+}
