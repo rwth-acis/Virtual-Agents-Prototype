d9a377e62e31ba39ce4e24236f5128e6373be674
Danylo Bekhter <danylo.bekhter@rwth-aachen.de>
2021-10-08 10:11:21 +0200

    Implemented an interface for complex agent tasks
    Complex agent tasks include subtasks

--- Assets/Virtual Agents Framework/Runtime/Scripts/TaskSystem/BasicAgentTasks/IAgentTask.cs
+++ Assets/Virtual Agents Framework/Runtime/Scripts/TaskSystem/BasicAgentTasks/IAgentTask.cs
@@ -29,5 +29,44 @@ namespace VirtualAgentsFramework
             // Fire when the task is finished to let the agent know
             event Action OnTaskFinished;
         }
+
+        /// <summary>
+        /// An internal AgentTask subtask-queue in addition to
+        /// the universal AgentTask methods and attributes
+        /// </summary>
+        public interface IAgentComplexTask : IAgentTask
+        {
+            // Any AgentTask can be scheduled as a subtask, including complex AgentTasks
+            AgentTaskManager subTaskQueue
+            {
+                get;
+                set;
+            }
+
+            // Current subtask execution state
+            State currentState
+            {
+                get;
+                set;
+            }
+
+            IAgentTask currentSubTask
+            {
+                get;
+                set;
+            }
+
+            void RequestNextSubTask(); //TODO create a default implementation
+
+            void OnSubTaskFinished(); //TODO create a default implementation
+        }
+
+        public enum State
+        {
+            inactive, // i.e. requesting new subtasks is disabled
+                      // (for testing purposes or basic tasks)
+            idle, // i.e. requesting new subtasks is enabled
+            busy // i.e. currently executing a subtask
+        }
     }
 }
