61c6d35d205b79f4406c820f38832ba0c2cba10d
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2022-01-24 15:16:47 +0100

    Animation updater now overrieds all agent movment exertet by animations and updates animation depeding on self observed speeds

--- Assets/Virtual Agents Framework/Runtime/Scripts/AgentAnimationUpdater.cs
+++ Assets/Virtual Agents Framework/Runtime/Scripts/AgentAnimationUpdater.cs
@@ -11,28 +11,55 @@ public class AgentAnimationUpdater : MonoBehaviour
     CharacterController controller;
     NavMeshAgent agent;
 	Animator animator;
+	NavMeshAgent navMeshAgent;
+
+	private Vector2 previousForwardVector;
+	private Vector3 previousPosition;
+	private GameObject target;
+
+	//Animation Parameter Names
+
+	[SerializeField] private string forwardSpeed = "Speed";
+	[SerializeField] private string angularSpeed = "Turn";
+	//The speed where the full running anmation is synchrone with the floor
+	[SerializeField] private float movmentTopSpeed = 4;
+	[SerializeField] private float angularTopSpeed = 20;
+
+	public bool movementHandeldByAnimator = false;
 
 	// animation IDs
 	private int _animIDSpeed;
-	private int _animIDGrounded;
-	private int _animIDJump;
-	private int _animIDFreeFall;
-	private int _animIDMotionSpeed;
+	private int _animIDAngularSpeed;
 
 	private void Awake()
 	{
 		AssignAnimationIDs();
 		agent = GetComponent<NavMeshAgent>();
 		animator = GetComponent<Animator>();
+		navMeshAgent = GetComponent<NavMeshAgent>();
 	}
 
     private void AssignAnimationIDs()
 	{
-		_animIDSpeed = Animator.StringToHash("Speed");
-		_animIDGrounded = Animator.StringToHash("Grounded");
-		_animIDJump = Animator.StringToHash("Jump");
-		_animIDFreeFall = Animator.StringToHash("FreeFall");
-		_animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
+		_animIDSpeed = Animator.StringToHash(forwardSpeed);
+		_animIDAngularSpeed = Animator.StringToHash(angularSpeed);
+	}
+
+	public void startMovement(GameObject target)
+	{
+		this.target = target;
+		navMeshAgent.SetDestination(target.transform.position);
+
+		navMeshAgent.updateRotation = true;
+		navMeshAgent.updatePosition = true;
+	}
+
+	public void stopMovement()
+	{
+		target = null;
+
+		navMeshAgent.updateRotation = false;
+		navMeshAgent.updatePosition = false;
 	}
 
 	/// <summary>
@@ -40,8 +67,30 @@ public class AgentAnimationUpdater : MonoBehaviour
 	/// </summary>
 	public void updateAnimatiorParameters()
 	{
-		// update animator if using character
-		animator.SetFloat(_animIDSpeed, agent.desiredVelocity.magnitude);
-		animator.SetFloat(_animIDMotionSpeed, 1);
+		//Update forward speed
+		float speed = (transform.position - previousPosition).magnitude / Time.deltaTime;
+		previousPosition = transform.position;
+		animator.SetFloat(_animIDSpeed, speed / movmentTopSpeed, 0.1f, Time.deltaTime);
+
+		//Update angeluar speed
+		Vector2 currentForwarVector = new Vector2(agent.transform.forward.x, agent.transform.forward.z);
+		float curSpeed = Vector2.SignedAngle(currentForwarVector,previousForwardVector) / Time.deltaTime;
+		previousForwardVector = currentForwarVector;
+		animator.SetFloat(_animIDAngularSpeed, curSpeed/ angularTopSpeed, 0.1f, Time.deltaTime);
+		Debug.Log(curSpeed);
+	}
+
+    private void Update()
+    {
+		if (target != null && navMeshAgent.destination != target.transform.position)
+		{
+			navMeshAgent.SetDestination(target.transform.position);
+		}
+		updateAnimatiorParameters();
+    }
+
+	public void OnAnimatorMove()
+	{
+		//Override, to disabel all movment from the animator, otherwise it would interfere with the navmeshAgent movement.
 	}
 }
